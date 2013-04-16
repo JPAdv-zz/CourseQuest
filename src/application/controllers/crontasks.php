65f85d35a2475f3c95e2816f40f2e639914acc56
= <=>
2013-04-16 11:40:10 -0700

    cron tasks controller added.  coursera scraping function added.

--- src/application/controllers/crontasks.php
+++ src/application/controllers/crontasks.php
@@ -4,6 +4,17 @@ class CronTasks extends CI_Controller {
 
     function scrape_coursera(){
         echo "scraping coursera....";
+
+        $urls = (object) array("coursesJson" => "https://d1hpa2gdx2lr6r.cloudfront.net/maestro/api/topic/list2");
+
+        $coursesJson = json_decode(file_get_contents($urls->coursesJson));
+
+        $topics = $coursesJson->topics;
+
+        foreach($topics as $topic){
+            echo $topic->short_name;
+            echo "<br/>";
+        }
     }
 
 }
