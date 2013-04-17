f291d6e37bcf05cf8df907bb03c15a9cd1dd5892
= <=>
2013-04-16 20:54:34 -0700

    limit removed

--- src/application/controllers/crontasks.php
+++ src/application/controllers/crontasks.php
@@ -3,8 +3,6 @@ class CronTasks extends CI_Controller {
 
     function scrape_coursera(){
 
-        set_time_limit(600);
-
         $this->load->model("course_data");
         $this->load->model("courseDetails");
         echo "scraping coursera....";
@@ -19,38 +17,41 @@ class CronTasks extends CI_Controller {
             $courseJson = json_decode(file_get_contents("https://www.coursera.org/maestro/api/topic/information?topic-id=".$topic->short_name));
             $instructorJson = json_decode(file_get_contents("https://www.coursera.org/maestro/api/user/instructorprofile?exclude_topics=1&topic_short_name=".$topic->short_name));
 
-
-            $catStr = "";
-            foreach($courseJson->categories as $cat){
-                if($catStr == ""){
-                    $catStr .= $cat->name;
-                }else{
-                    $catStr .= ", ".$cat->name;
-                }
-            }
-
-            $courseJson->category = $catStr;
-
-            if($courseJson->video == ""){
-                $courseJson->video_link = "";
-            }else{
-                $courseJson->video_link = "http://www.youtube.com/watch?v=".$courseJson->video;
-            }
-
-
-            foreach($courseJson->courses as $course){
-                if($course->start_year){
-                    $courseJson->start_date = $course->start_year."-".str_pad($course->start_month,2,'0', STR_PAD_LEFT)."-".str_pad($course->start_day,2,'0', STR_PAD_LEFT);
-                    $courseJson->course_length = $course->duration_string;
-                    $courseJson->site = $course->home_link;
-                    $id = $this->course_data->insertRow($courseJson);
-
-                    foreach($instructorJson as $instructor){
-                        $instructor->id = $id;
-                        $this->courseDetails->insertRow($instructor);
+            if($courseJson->social_link){
+                $catStr = "";
+                foreach($courseJson->categories as $cat){
+                    if($catStr == ""){
+                        $catStr .= $cat->name;
+                    }else{
+                        $catStr .= ", ".$cat->name;
                     }
-
                 }
+
+                $courseJson->category = $catStr;
+
+                if($courseJson->video == ""){
+                    $courseJson->video_link = "";
+                }else{
+                    $courseJson->video_link = "http://www.youtube.com/watch?v=".$courseJson->video;
+                }
+
+
+                foreach($courseJson->courses as $course){
+                    if($course->start_year){
+                        $courseJson->start_date = $course->start_year."-".str_pad($course->start_month,2,'0', STR_PAD_LEFT)."-".str_pad($course->start_day,2,'0', STR_PAD_LEFT);
+                        $courseJson->course_length = $course->duration_string;
+                        $courseJson->site = $course->home_link;
+                        $id = $this->course_data->insertRow($courseJson);
+
+                        foreach($instructorJson as $instructor){
+                            $instructor->id = $id;
+                            $this->courseDetails->insertRow($instructor);
+                        }
+
+                    }
+                }
+
+                echo "done";
             }
         }
     }
