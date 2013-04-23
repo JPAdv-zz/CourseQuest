5015b64fbeb49187964e620828cff2ef3bbd0ed2
Jose Padilla <jose.padilla.88@gmail.com>
2013-04-22 22:50:49 -0700

    Update connection.php

--- src/external_libraries/Canvas Scrapper/moocs/connection.php
+++ src/external_libraries/Canvas Scrapper/moocs/connection.php
@@ -1,7 +1,7 @@
 <?php
 $mysql_hostname = "localhost";
 $mysql_user = "root";
-$mysql_password = "jp!db160";
+$mysql_password = "";
 $mysql_database = "moocs";
 $prefix = "";
 $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
