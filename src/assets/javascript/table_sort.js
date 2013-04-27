44116b72c51eab5c5b78bcebef4a885ab122ef92
Jose Padilla <jose.padilla.88@gmail.com>
2013-04-27 02:36:34 -0700

    Added Sortable Table

--- src/assets/javascript/table_sort.js
+++ src/assets/javascript/table_sort.js
@@ -1,7 +1,13 @@
 /*
-$(document).ready(function()
-	{
-		$("#tablecontainer").tablesorter();
-	}
-);
-*/
+  This is the function that sorts the table.
+  @Author: Jose Padilla
+ */
+$(document).ready(function() {
+		// Calls the tablesorter plugin.
+		$("#courseTable").tablesorter({
+		// Sort by # of weeks.
+		sortList: [[4,1]],
+		// Do not sort by images (both courses and professors)!
+		headers: { 0:{sorter: false}, 6:{sorter:false}}
+	});
+});
