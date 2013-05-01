a355085267d4f0721c1a3cd2a942d086afa2561b
kashyap patel <kashyap02004@github.com>
2013-04-30 22:57:17 -0700

    search added

---
+++ src/application/views/table_view_q.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ This is a function that filters the string returned by the table.
+*/
+function filterRecord($s){
+
+    // Check that the string is only a number.
+    // Length of class.
+    if(is_numeric($s)) {
+        return $s." weeks";
+    }
+
+    //Check that the string contains the characters http.
+    if(strpos($s, "http") !== false) {
+        // Check if the string is from coursera. If so, set it to return the string "Coursera" only.
+        if(strpos($s, "class.coursera.org") !== false){
+            return $s="Coursera";
+        }
+
+//        if (strpos($s, "large-icon.png") !== false) {
+//            $imageClass = "course-image";
+//        }
+//        else {
+//            $imageClass = "professor-image";
+//        }
+//        return "<img class='an-tr-op ".$imageClass."' onload='this.style.opacity = 1;' src='".$s."'>";
+    }
+    else{
+        return $s;
+    }
+
+    return $s;
+}
+?>
+
+    <?    echo $this->table->generate($records); ?>
