b868ea98a0a7347ac53133a330d271b3738a9e11
Virender Kang <virenderkang71@gmail.com>
2013-05-02 19:15:16 -0700

    scrapper fixed to trim the string before storing them to db.

--- src/application/models/course_data.php
+++ src/application/models/course_data.php
@@ -5,16 +5,16 @@ class course_data extends CI_Model {
     function insertRow($o){
 
         $data = array(
-            "title" => $o->name,
-            "short_desc" => $o->short_description,
-            "long_desc" => $o->about_the_course,
-            "course_link" => $o->social_link,
-            "video_link" => $o->video_link,// need to add -----
-            "start_date" => $o->start_date,// need to add -----
-            "course_length" => $o->course_length, // need to add ------
-            "course_image" => $o->large_icon,
-            "category" => $o->category, //need to add -------
-            "site" => $o->site, //need to add
+            "title" => trim($o->name),
+            "short_desc" => trim($o->short_description),
+            "long_desc" => trim($o->about_the_course),
+            "course_link" => trim($o->social_link),
+            "video_link" => trim($o->video_link),// need to add -----
+            "start_date" => trim($o->start_date),// need to add -----
+            "course_length" => trim($o->course_length), // need to add ------
+            "course_image" => trim($o->large_icon),
+            "category" => trim($o->category), //need to add -------
+            "site" => trim($o->site), //need to add
         );
 
         $this->db->insert('course_data',$data);
