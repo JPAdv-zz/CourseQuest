585142eeb050210a7863b3b86925bdee8e2b3dd0
Tung Dang <tungqd90@yahoo.com>
2013-05-03 12:03:50 -0700

    fix layout

--- src/application/views/templates/header_template.php
+++ src/application/views/templates/header_template.php
@@ -9,10 +9,10 @@
             <script src="<?php echo base_url(); ?>assets/javascript/jquery-ui.js" type="text/javascript"></script>
             <link rel="stylesheet" href="<?php echo base_url(); ?>assets/stylesheets/costum.css" type="text/css" media="screen" charset="utf-8"/>
             <link rel="stylesheet" href="<?php echo base_url(); ?>assets/stylesheets/table_view.css" type="text/css" media="screen" charset="utf-8"/>
-            <link rel="stylesheet" href="<?php echo base_url(); ?>assets/stylesheets/gridviewlistview.css" type="text/css" media="screen" charset="utf-8"/>
-            <link rel="stylesheet" href="<?php echo base_url(); ?>assets/stylesheets/home_view.css" type="text/css" media="screen" charset="utf-8"/>
+            <link rel="stylesheet" href="<?php echo base_url(); ?>assets/stylesheets/gridviewlistview.css" type="text/css" media="screen" charset="utf-8"/>      
             <link rel="stylesheet" href="<?php echo base_url(); ?>assets/stylesheets/jquery-ui.css" type="text/css" media="screen" charset="utf-8"/>
             <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,300' rel='stylesheet' type='text/css'>
+            <link rel="stylesheet" href="<?php echo base_url(); ?>assets/stylesheets/home_view.css" type="text/css" media="screen" charset="utf-8"/>
         </head>
 
         <script>
