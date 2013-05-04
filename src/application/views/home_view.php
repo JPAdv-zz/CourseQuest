ee6eae6e295130a86db1b80c430bf2d34f2127f3
kashyap patel <kashyap02004@github.com>
2013-05-04 00:56:41 -0700

    style changes

--- src/application/views/home_view.php
+++ src/application/views/home_view.php
@@ -100,7 +100,7 @@
                 var elem = document.createElement('div');
                 elem.className = 'grid_result_item';
                 elem.innerHTML ="<div class='play-button-container'><div onclick='loadPreviewVideo(\""+data[i].video_link+"\")' class='play-button'></div></div>"+
-                    "<img class='result_img an-tr-op' onload='this.style.opacity = 1;' onclick='loadPreviewVideo(\""+data[i].video_link+"\");' src='"+data[i].course_image+"'/><div class='result_details'><a href='"+data[i].course_link+"' class='result_title'>"+data[i].title+"</a><strong style='float:right;cursor:pointer;' title='Description: "+data[i].short_desc+" / Start Date is "+data[i].start_date+". / Duration is "+data[i].course_length+" weeks.'><em>i</em></strong></div>";
+                    "<img class='result_img an-tr-op' onload='this.style.opacity = 1;' onclick='loadPreviewVideo(\""+data[i].video_link+"\");' src='"+data[i].course_image+"'/><div class='result_details'><a href='"+data[i].course_link+"' class='result_title'>"+data[i].title+"</a><strong class='grid_view_more_info' title='Description: "+data[i].short_desc+" / Start Date is "+data[i].start_date+". / Duration is "+data[i].course_length+" weeks.'>i</strong></div>";
 
                 df.appendChild(elem);
             }
